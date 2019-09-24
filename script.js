702a811860b81d095afc51d94202fbb3b5862b4c
Ramon Sanchez <songwright66@gmail.com>
2019-09-23 18:43:18 -0700

    Add questions array

--- script.js
+++ script.js
@@ -8,6 +8,20 @@ startButton.addEventListener('click', function (event) {
   startCard.setAttribute("style", "display: none")
 });
 
+let questions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  }
+  ///etc.
+];
+
 // Timer functions based on a timer by Daniel Hug https://jsfiddle.net/Daniel_Hug/pvk6p/
 // function tick() {
 //   seconds +=1;
