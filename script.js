3e44d1614dc14f0a21dea6771a8d00367f70e62a
Ramon Sanchez <songwright66@gmail.com>
2019-09-23 18:42:30 -0700

    Add variables and event listener for start button

---
+++ script.js
@@ -0,0 +1,33 @@
+let startButton = document.querySelector("#start-quiz-btn");
+let startCard = document.querySelector("#start-card");
+let quizCard = document.querySelector("#quiz-card");
+
+startButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  quizCard.setAttribute("style", "display: flex")
+  startCard.setAttribute("style", "display: none")
+});
+
+// Timer functions based on a timer by Daniel Hug https://jsfiddle.net/Daniel_Hug/pvk6p/
+// function tick() {
+//   seconds +=1;
+//   if (seconds >= 60) { // The hexigesimal number base for seconds converts them to minutes at 60 seconds.
+//       seconds = 0;
+//       minutes +=1;
+//   }
+//   clock.textContent = (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+//   timer();
+// }
+
+// function timer() {
+//   t = setTimeout(tick, 1000);
+// }
+
+// function stopClock () {
+//   clearTimeout(t);
+// }
+
+// function clearClock () {
+//   clock.textContent = "00:00";
+//       seconds = 0; minutes = 0;
+// }
