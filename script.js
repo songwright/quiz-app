d6001b369c5aea61e24531bc2700776a83f9eba6
Ramon Sanchez <songwright66@gmail.com>
2019-09-24 19:29:40 -0700

    Update renderQuestion function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let startButton = document.querySelector("#start-quiz-btn");
 let startCard = document.querySelector("#start-card");
 let quizCard = document.querySelector("#quiz-card");
+let finalCard = document.querySelector("#final-card");
 let questionTitle = document.querySelector("#title");
 let choiceList = document.querySelector(".list-group");
 let quizMessage = document.querySelector("#quiz-message");
@@ -30,26 +31,32 @@ let score = 0;
 let questionNum = 0;
 
 function renderQuestion() {
+  // Reset the list of multiple choice answers.
   choiceList.innerHTML = '';
-  questionTitle.textContent = questions[questionNum].title;
+  
+  if (questionNum === questions.length ) {
+    finalCard.setAttribute("style", "display: flex")
+    quizCard.setAttribute("style", "display: none")
+  } else {
+    for (let i = 0; i < questions[questionNum].choices.length; i++) {
+      let li = document.createElement("li");
+      li.textContent = questions[questionNum].choices[i];
+      // li.setAttribute("class", "list-group-item");
+      choiceList.appendChild(li);
+  
+      li.addEventListener("click", function(event) {
+        let choice = event.target;
+  
+          if (choice.innerHTML === questions[questionNum].answer) {
+            questionNum++;
+            renderQuestion();
+          } else if (choice !== questions[questionNum].answer) {
+            takeScore();
+          }
+      });
+    }
 
-  for (i = 0; i < questions[questionNum].choices.length; i++) {
-    let li = document.createElement("li");
-    li.textContent = questions[questionNum].choices[i];
-    // li.setAttribute("class", "list-group-item");
-    choiceList.appendChild(li);
-
-    li.addEventListener("click", function(event) {
-      let choice = event.target;
-
-        if (choice.innerHTML === questions[questionNum].answer) {
-          questionNum++;
-          console.log("questionNum = " + questionNum);
-          renderQuestion();
-        } else if (choice !== questions[questionNum].answer) {
-          takeScore();
-        }
-    });
+    questionTitle.textContent = questions[questionNum].title;
   }
 }
 
